e4b4650fb2c19a2a1a609e328ded018698988295
pinzii <fpinzon514@gmail.com>
2025-08-07 04:24:52 -0500

    Registra store, devTools y effects en arranque - app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,11 @@ import { provideHttpClient } from '@angular/common/http';
 import { withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
 import { routes } from './app.routes';
+import { provideStore }         from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects }       from '@ngrx/effects';
+import { productReducer }       from './state/product.reducer';
+import { ProductEffects }       from './state/product.effects';
 
 
 export const appConfig: ApplicationConfig = {
@@ -11,5 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),   
     provideHttpClient(
       withInterceptors([authInterceptor])),
-     provideRouter(routes)]
+     provideRouter(routes),
+    // NGRX Store and Effects
+    provideStore({ product: productReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: false }),
+    provideEffects([ ProductEffects ])
+  ]
 };
